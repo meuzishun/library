efc1181aee6e9675608e3509163410e209003dec
Andrew Smith <meuzishun@gmail.com>
2021-07-20 12:34:04 -0400

    Create first JS experiments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const visualLibrary = document.querySelector('.visual-library');
 let library = [];
 
 function Book(title, author, pages, read) {
@@ -13,3 +14,43 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book) {
     library.push(book);
 }
+
+// for adding books to HTML
+function displayBooks() {
+    visualLibrary.textContent = '';
+
+    library.forEach(book => {
+        const bookContainer = document.createElement('div');
+        const bookInfo = document.createElement('div');
+        const bookTitle = document.createElement('p');
+        const bookAuthor = document.createElement('p');
+        const bookPages = document.createElement('p');
+        const statusButton = document.createElement('button');
+
+        bookContainer.classList.add('book-container');
+        bookInfo.classList.add('book-info');
+        bookTitle.classList.add('title');
+        bookAuthor.classList.add('author');
+        bookPages.classList.add('pages');
+        statusButton.classList.add('status');
+
+        bookTitle.textContent = book.title;
+        bookAuthor.textContent = `Author: ${book.author}`;
+        bookPages.textContent = `Length: ${book.pages} pages`;
+        statusButton.textContent = `Mark as Read`;
+
+        bookInfo.appendChild(bookTitle);
+        bookInfo.appendChild(bookAuthor);
+        bookInfo.appendChild(bookPages);
+
+        bookContainer.appendChild(bookInfo);
+        bookContainer.appendChild(statusButton);
+
+        visualLibrary.appendChild(bookContainer);
+    });
+}
+
+let test1 = new Book('Python', 'Some dude', 550, true);
+
+addBookToLibrary(test1);
+displayBooks();
